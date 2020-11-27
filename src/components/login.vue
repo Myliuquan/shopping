2a5bfeb3ea3a4e5f9d6f723618786602b74b0beb
Myliuquan <775375987@qq.com>
2020-11-27 16:47:10 +0800

    login

---
+++ src/components/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login_container">
+    主体区域
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b5b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
