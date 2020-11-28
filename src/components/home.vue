bcdf623e9b91bbc047881147d13ff0e48fec6c39
Myliuquan <775375987@qq.com>
2020-11-28 15:13:29 +0800

    login

---
+++ src/components/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header
+      >Header <el-button type="info" @click="logout">退出</el-button></el-header
+    >
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
